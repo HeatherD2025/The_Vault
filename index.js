8e7b3651eca1454c7d0949ff8294da1f7f0bb3cb
heatherd2025 <heatherdeliso@gmail.com>
2025-03-14 12:44:18 -0400

    adding html file

---
+++ index.js
@@ -0,0 +1,20 @@
+// Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/). Each must equal one of the three codes in the combination.
+// Add comments throughout the code to explain each step in the process.
+// Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+// Please submit either a file with your code or a screenshot of the code, with the pseudocode showing, or a GitHub link.
+
+//Asing armithmetic operators to express the combination
+const a = 5 + 5;
+const b = 80 / 2;
+const c = 40 - 1;
+// These set the values for  a, b, and c using arithmetic operators to get the combination code
+
+//send a message to the user that expresses the combination
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${a} ${b} ${c}`);
+// This creates a message telling the user the message and combination
+
+
+
+
+
